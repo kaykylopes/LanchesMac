a62a31d93d7b350d8f87a32cba26e9ccb2c58e1d
kayky lopes <kaykylopes@hotmail.com>
2019-12-12 22:01:14 -0300

    Criando Controller e View Model do Carrinho de Compra

---
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanchesMac.Models;
+using LanchesMac.Repositories;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(ILancheRepository lancheRepository,
+                                        CarrinhoCompra carrinhoCompra)
+        {
+            _lancheRepository = lancheRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+
+        public IActionResult Index()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+
+            var carrinhoCompraViewModel = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+        };
+            return View(carrinhoCompraViewModel); ;
+        }
+
+        public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+                if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoverItemNoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+                if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
