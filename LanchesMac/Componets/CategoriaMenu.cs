2b829ee06de5faeea9839a6f7fdddabe628e9b69
kayky lopes <kaykylopes@hotmail.com>
2019-12-23 12:51:05 -0300

    Criando Menu da Categoria do Lanche

---
+++ LanchesMac/Componets/CategoriaMenu.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesMac.Componets
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaReposity;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaReposity = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaReposity.Categorias.OrderBy(c =>
+                c.CategoriaNome);
+
+            return View(categorias);
+        }
+    }
+}
